a73b8198c803a9f3d78c4f755a22dd7ca91e0a00
maxcrimea <maxcrimea50@gmail.com>
2023-11-16 11:15:09 +0600

    fixed links to files

--- web/fileconverter/models.py
+++ web/fileconverter/models.py
@@ -5,8 +5,8 @@ from users.models import User
 
 class ConvertedModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
-    original_file = models.FileField(verbose_name='Исходный файл')
-    converted_file = models.FileField(verbose_name='Сконвертированный файл', blank=True, null=True)
+    original_file = models.FileField(upload_to='original_files', verbose_name='Исходный файл')
+    converted_file = models.FileField(upload_to='converted_files',verbose_name='Сконвертированный файл', blank=True, null=True)
     converted_filename = models.CharField(max_length=255, verbose_name='Имя сконвертированного файла', blank=True)
     timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
--- web/fileconverter/views.py
+++ web/fileconverter/views.py
@@ -1,7 +1,7 @@
-from django.conf import settings
 import os
 import subprocess
 
+from django.conf import settings
 from django.utils.dateparse import parse_date
 from rest_framework import status
 from rest_framework.pagination import LimitOffsetPagination
@@ -36,55 +36,47 @@ class ConvertedModelListCreateView(ListCreateAPIView):
     def perform_create(self, serializer):
         original_file = self.request.FILES.get('original_file')
 
-        # Check if original_file is present in the request
         if not original_file:
             return Response("No file provided", status=status.HTTP_400_BAD_REQUEST)
 
-        # Save original file in original_files/ directory
-        original_files_dir = os.path.join('original_files')
-        os.makedirs(original_files_dir, exist_ok=True)
-        original_file_path = os.path.join(original_files_dir, original_file.name)
+        # Создание экземпляра модели напрямую сохранит файл в нужном месте
+        converted_model = ConvertedModel.objects.create(
+            user=self.request.user,
+            original_file=original_file
+        )
 
-        with open(original_file_path, 'wb') as destination:
-            for chunk in original_file.chunks():
-                destination.write(chunk)
+        # Получение абсолютного пути к файлу
+        original_file_path = converted_model.original_file.path
 
         try:
-            # Create the directory for converted files if it doesn't exist
-            converted_files_dir = os.path.join('converted_files')
+            # Конвертация файла
+            converted_files_dir = os.path.join(settings.MEDIA_ROOT, 'converted_files')
             os.makedirs(converted_files_dir, exist_ok=True)
-
-            # Define paths for conversion
-            output_file = os.path.join(converted_files_dir, os.path.splitext(original_file.name)[0] + '.glb')
+            output_file_name = os.path.splitext(original_file.name)[0] + '.glb'
+            output_file_path = os.path.join(converted_files_dir, output_file_name)
 
             blender_executable = "blender"
             blender_script = os.path.join('fileconverter', 'blender_script.py')
 
-            # Convert the model using Blender
             try:
                 subprocess.run([blender_executable, "--background", "--python", blender_script, original_file_path,
-                                output_file], check=True)
+                                output_file_path], check=True)
             except subprocess.CalledProcessError:
                 print(f"...")
 
-            # Save the converted model in the database
+            if not os.path.exists(output_file_path):
+                raise Exception(f"Converted file not found: {output_file_path}")
 
-            if not os.path.exists(output_file):
-                raise Exception(f"Converted file not found: {output_file}")
-
-            try:
-                converted_model = ConvertedModel.objects.create(
-                    user=self.request.user,
-                    original_file=original_file_path,
-                    converted_file=output_file,
-                    converted_filename=os.path.basename(output_file)
-                )
-                serializer.instance = converted_model
-            except Exception as e:
-                print(f"Ошибка при создании модели: {e}")
+            # Обновление экземпляра модели с конвертированным файлом
+            converted_model.converted_file = 'converted_files/' + output_file_name
+            converted_model.converted_filename = output_file_name
+            converted_model.save()
 
         except Exception as e:
+            converted_model.delete()  # Удаляем запись при неудачной конвертации
             return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        serializer.instance = converted_model
 
 
 class ConvertedModelDetailView(RetrieveDestroyAPIView):
